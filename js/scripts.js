b7709219f035b43a5976184e5db742be2b07b881
parkerbenoit <72704200+parkerbenoit@users.noreply.github.com>
2020-10-23 09:20:54 -0700

    show 5 form groups and now has button feature

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,6 @@ $(document).ready(function() {
   $("form#selector").submit(function (event) {
     event.preventDefault();
     const question1 = parseInt($("input#question1").val());
-    const question2 = 
-    const question3 = 
+    const question2 = parseInt($("input#question2").val());
   }
 }
