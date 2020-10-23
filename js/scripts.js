c2ad51054f5ed9f13dec2a2284914a684daae2e6
parkerbenoit <72704200+parkerbenoit@users.noreply.github.com>
2020-10-23 13:09:33 -0700

    has image sizes fixed and header centered

--- js/scripts.js
+++ js/scripts.js
@@ -10,13 +10,13 @@ $(document).ready(function() {
 
     if (question1 === question2 || question1 === question3 || question1 === question4 || question1 === question5 || question2 === question3 || question2 === question4 || question2 === question5 || question3 === question4 || question3 === question5 || question4 === question5) {
       $("#output1").show();
-    } else if (question1 > question2) {
+    } else if (question4 > question5) {
       $("#output3").show();
     } else if (question5 > 5) {
       $("#output2").show();
     } else if (question4 = 0) {
       $("#output1").show();
-    } else if (question2 > 6) {
+    } else if (question2 > 0) {
       $("#output2").show();
     }
   });
